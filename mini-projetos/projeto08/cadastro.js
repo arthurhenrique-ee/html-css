e5faa2f86d4f07054f2fa3d85136fe77c3e2c9cb
Arthur Henrique Gunes <art123.gunesmarciano@gmail.com>
2025-04-09 17:17:07 -0300

    ihy

--- mini-projetos/projeto08/cadastro.js
+++ mini-projetos/projeto08/cadastro.js
@@ -1,5 +1,18 @@
 let btnCadastro = document.querySelector('.btn-cad')
+let inputs = document.querySelectorAll('.cad-form input, .cad-form select')
+
+email = document.querySelector('input#email')
 
 btnCadastro.addEventListener('click', () => {
-    
+
+    function emailValid() {
+        return /[@]/.test(email.value)
+    }
+
+    if (emailValid()) {
+        alert('Tudo certo')
+    } else {
+        alert('Email Inválido')
+    }
+
 })
