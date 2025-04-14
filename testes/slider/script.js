5907900bbe859e22fe95f9f9df10dcc7bba9e0aa
Arthur Henrique Gunes Marciano <151869332+arthurhenrique-ee@users.noreply.github.com>
2025-04-14 17:44:39 -0300

    Slider
    Quase pronto (vou apenas mudar as imagens)

---
+++ testes/slider/script.js
@@ -0,0 +1,23 @@
+const slides = document.querySelectorAll('.slide')
+let cont = 0
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'))
+    slides[index].classList.add('active')
+}
+
+document.getElementById('prev').addEventListener('click', () => {
+    cont -= 1
+    if (cont < 0) {
+        cont = slides.length - 1
+    }
+    showSlide(cont)
+  });
+  
+  document.getElementById('next').addEventListener('click', () => {
+    cont += 1;
+    if (cont >= slides.length) {
+        cont = 0
+    }
+    showSlide(cont)
+  });
