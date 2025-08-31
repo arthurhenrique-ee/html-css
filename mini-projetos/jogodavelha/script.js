ad3ced5698a82b1f1a2e0f79c7ea149eae500a3a
Arthur Henrique Gunes <art123.gunesmarciano@gmail.com>
2025-08-31 17:42:10 -0300

    Reorganização

--- mini-projetos/jogodavelha/script.js
+++
@@ -1,9 +0,0 @@
-let cells = document.querySelectorAll('.cel');
-
-cells.forEach(cel => {
-    cel.addEventListener('click', () => {
-        if (!cel.classList.contains('x') && !cel.classList.contains('circle')) {
-            cel.classList.add('x');
-        }
-    });
-});
